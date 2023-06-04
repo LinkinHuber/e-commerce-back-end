8442b4d99400ace8c7d0c4889ad517d806093a38
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-04 00:01:04 -0500

    finished all the routes for the category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,80 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const inquireCategory = await Category.findAll( {
+      include: [{ model: Product}]
+    });
+    res.status(200).json(inquireCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const inquireCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+
+    if (!inquireCategory) {
+      res.status(404).json({ message: 'Invalid category id' });
+      return;
+    }
+
+    res.status(200).json(inquireCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const inquireCategory = await Category.create(req.body);
+    res.status(200).json(inquireCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    req.body, 
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const inquireCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+  
+    if (!inquireCategory) {
+      res.status(404).json({ message: 'Invalid category id'});
+      return;
+    }
+
+    res.status(200).json(inquireCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
